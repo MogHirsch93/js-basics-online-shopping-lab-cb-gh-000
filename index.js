a919e75d6b097803a42b55b40d073becfff6de4b
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-14 15:09:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,16 +9,43 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
-}
+function addToCart(itemName) {
+  let itemPrice = Math.floor((Math.random() * 100) +1);
+  let item = {[itemName]: itemPrice};
+  console.log (`${itemName} has been added to your cart.`);
+  cart.push(item);
+  console.log(cart);
+  return cart;
+  }
 
 function viewCart() {
-  // write your code here
+  let currentCart = 'In your cart, you have ';
+  if(cart.length === 0){
+    console.log ("Your shopping cart is empty.");
+  }
+  for(let i = 0; i < cart.length; i++){
+    if(i === cart.length-1){
+      currentCart += `${Object.keys(cart[i])[0]} at $${Object.values(cart[i])[0]}.`
+    }if(i === cart.length-2){
+      currentCart += `${Object.keys(cart[i])[0]} at $${Object.values(cart[i])[0]} and `
+    }else{
+      currentCart += `${Object.keys(cart[i])[0]} at $${Object.values(cart[i])[0]}`
+    }
+  }
+  console.log(currentCart);
+}
+
+// Reduce takes in the accumulating value, the current value and the starting number.
+// Object.values will return each item object's value as an individual array.
+// We then find the number that is in that array by looking at its index ([i])
+// We then add this number value to the accumulating total of values and we have
+// our cart total.
+function sumArray(acc, cur) {
+  return acc + Object.values(cur)[0]
 }
 
 function total() {
-  // write your code here
+  return cart.reduce(sumArray, 0);
 }
 
 function removeFromCart(item) {
