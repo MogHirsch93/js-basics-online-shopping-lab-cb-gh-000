115b7c4c8e1a8e9a19ff25cbc036094825b38719
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-14 16:49:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,4 +62,9 @@ function removeFromCart(itemName) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(cardNumber === undefined){
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }else{
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}. `
+  }
 }
